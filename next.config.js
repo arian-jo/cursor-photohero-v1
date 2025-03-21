3cc55118dc00706f67c2ab2724679811207b681a
arian-jo <arian.asdfghqwerty@gmail.com>
2025-03-21 10:41:20 -0300

    Fix Netlify build: Configure webpack to properly resolve Firebase modules

--- next.config.js
+++ next.config.js
@@ -8,8 +8,23 @@ const nextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'firebase/app': require.resolve('firebase/app'),
+      'firebase/auth': require.resolve('firebase/auth'),
+      'firebase/analytics': require.resolve('firebase/analytics'),
+    };
+    return config;
   },
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
